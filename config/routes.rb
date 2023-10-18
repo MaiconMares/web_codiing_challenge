4b7c68e006fa1a92eb505301f766ff9b8b3c04c9
MaiconMares <maiconmaresunb@gmail.com>
2023-10-17 23:14:46 -0300

    Fixing routes conflicts.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  
-  resources :conferences
-  resources :schedulings
-
   root "conferences#index"
   
+  resources :conferences, only: [:index]
+  resources :schedulings
+
+  get "conferences/new", to: "conferences#new", :as => :new_conference
+  get "conferences/edit/:id", to: "conferences#edit", :as => :edit_conference
+  delete "conferences/:id", to: "conferences#destroy", :as => :delete_conference
+  post "conferences/:id", to: "conferences#update"
+  post "conferences", to: "conferences#create"
+  get "conferences/:id", to: "conferences#show", :as => :conference
+  
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
